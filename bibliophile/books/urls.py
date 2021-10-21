7c2ed845ceab495c49557d7bfbec1ddd1b61e00f
sarath169 <rayala@iiitkalyani.ac.in>
2021-10-21 14:26:13 +0530

    added books app

--- bibliophile/books/urls.py
+++ bibliophile/books/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path, include
+from django.urls import path
 from .views import index
 urlpatterns = [
-    path('book/', include('books.urls')),
     path('', index, name = 'index' ),
 ]
