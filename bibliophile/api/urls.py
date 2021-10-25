4c38f30617404cb571208caf023b9bd9448afba2
sarath169 <rayala@iiitkalyani.ac.in>
2021-10-25 21:44:34 +0530

    api for profiles

--- bibliophile/api/urls.py
+++ bibliophile/api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 urlpatterns = [
     path('book/', include('books.urls')),
     path('', views.index, name = 'index' ),
-    path('profile/', views.ProfileView.as_view(), name= "profile" )
+    path('profile/', views.ProfileView.as_view(), name= "profile" ),
+    re_path(r'^edit/(?P<email>[^/]+)/$', views.UpdateProfileView, name='update_profile'),
+    path('editprofile/<slug:email>/', views.UpdateProfileView.as_view(), name="update_profile"),
 ]
