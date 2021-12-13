fc433ed14e11c5a25764d5c4afca1cea96f21cf9
sarath169 <rayala@iiitkalyani.ac.in>
2021-12-13 10:26:27 +0530

    corrected home.js

--- ui_bibliophile/src/pages/Home.js
+++ ui_bibliophile/src/pages/Home.js
@@ -9,7 +9,7 @@ import {
 } from "../helpers/BookAPICalles";
 import BookSkeleton from "../components/Skeleton/BookSkeleton";
 import { isAuthenticated } from "../helpers/AuthHelper";
-import defaultBook from '../images/default-book.jpg';
+import defaultBook from "../images/default-book.jpg";
 import ReviewCardSmall from "../components/ReviewCardSmall";
 
 const useStyles = makeStyles(() => ({
@@ -28,25 +28,23 @@ const useStyles = makeStyles(() => ({
     textAlign: "center",
     backgroundColor: "white",
   },
-  skeletonWraper:{
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
+  skeletonWraper: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
-  reviews:{
-    
-  },
+  reviews: {},
   reviewTitle: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: '15px',
-    display: 'block',
-    backgroundColor: '#dedede',
-    borderTopRightRadius: '8px',
-    borderTopLeftRadius: '8px',
-    padding: '5px 0px',
-    marginBottom: '5px',
-  }
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "15px",
+    display: "block",
+    backgroundColor: "#dedede",
+    borderTopRightRadius: "8px",
+    borderTopLeftRadius: "8px",
+    padding: "5px 0px",
+    marginBottom: "5px",
+  },
 }));
 
 const Home = () => {
@@ -56,13 +54,12 @@ const Home = () => {
   const [popularBooks, setPopularBooks] = useState([]);
   const [topRatedBooks, setTopRatedBooks] = useState([]);
   const [recommendedBooks, setRecommendedBooks] = useState([]);
+  const [reviews, setReviews] = useState([]);
   const bibId = localStorage.getItem("bib_id");
 
   useEffect(() => {
     setTimeout(() => {
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
+      
       getPopularBooks()
         .then((res) => {
           if (res) {
@@ -79,119 +76,118 @@ const Home = () => {
           }
         })
         .catch((err) => console.log(err));
-    }, 1000);
-      
-        getLatestReview()
+      getLatestReview()
         .then((res) => {
-          if(res){
+          if (res) {
             setReviews(res);
           }
         })
         .catch((err) => console.log(err));
+    }, 1000);
   }, []);
 
   useEffect(() => {
-    if(bibId){
-    isAuthenticated() &&
-      getRecommendedBooks()
-        .then((res) => {
-          console.log(res);
-          if (res) {
-            setLoading(false);
-            setRecommendedBooks(res);
-          }
-        })
-        .catch((err) => console.log(err));
+    if (bibId) {
+      isAuthenticated() &&
+        getRecommendedBooks()
+          .then((res) => {
+            console.log(res);
+            if (res) {
+              setLoading(false);
+              setRecommendedBooks(res);
+            }
+          })
+          .catch((err) => console.log(err));
     }
   }, [bibId]);
 
   return (
     <Container className={classes.container}>
-      {isAuthenticated() ? (<section>
-        <Typography variant="h5" className={classes.title}>
-          Recommendation
-        </Typography>
-        {loading && (
-          <div className={classes.skeletonWraper}>
-            {[1, 2, 3, 4, 5, 6].map((n) => (
-              <BookSkeleton key={n} />
-            ))}
-          </div>
-        )}
-        <Grid container spacing={2}>
-          {recommendedBooks.map((book) => {
-            return (
-              <Grid key={book.id} item xs={12} sm={4} md={2}>
-                <BookCard
-                  book={{
-                    id: book.id,
-                    image_link_small: book.volumeInfo.imageLinks
-                      ? book.volumeInfo.imageLinks.smallThumbnail
-                      : defaultBook,
-                    title: book.volumeInfo.title,
-                    googleSearch: true,
-                  }}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </section>) : (<></>)}
+      {isAuthenticated() ? (
+        <section>
+          <Typography variant="h5" className={classes.title}>
+            Recommendation
+          </Typography>
+          {loading && (
+            <div className={classes.skeletonWraper}>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <BookSkeleton key={n} />
+              ))}
+            </div>
+          )}
+          <Grid container spacing={2}>
+            {recommendedBooks.map((book) => {
+              return (
+                <Grid key={book.id} item xs={12} sm={4} md={2}>
+                  <BookCard
+                    book={{
+                      id: book.id,
+                      image_link_small: book.volumeInfo.imageLinks
+                        ? book.volumeInfo.imageLinks.smallThumbnail
+                        : defaultBook,
+                      title: book.volumeInfo.title,
+                      googleSearch: true,
+                    }}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </section>
+      ) : (
+        <></>
+      )}
       <Grid container spacing={1}>
         <Grid item lg={10}>
-      <section>
-        <Typography variant="h5" className={classes.title}>
-          Popular Books
-        </Typography>
-        {
-          loading && (
-            <div className={classes.skeletonWraper}>
-              {[1,2,3,4,5].map((n)=> <BookSkeleton key={n} />)}
-            </div>
-          )
-        }
-        <Grid container spacing={2}>
-          {popularBooks.map((book) => {
-            book["googleSearch"] = false;
-            return (
-              <Grid key={book.id} item xs={12} sm={3} >
-                <BookCard book={book} />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </section>
-
-      <section>
-        <Typography variant="h5" className={classes.title}>
-          Top Rated Books
-        </Typography>
-        {
-          loading && (
-            <div className={classes.skeletonWraper}>
-              {[1,2,3,4,5].map((n)=> <BookSkeleton key={n} />)}
-            </div>
-          )
-        }
-        <Grid container>
-          {topRatedBooks.map((book) => (
-            <Grid key={book.id} item xs={12} sm={3}>
-              <BookCard book={book} />
+          <section>
+            <Typography variant="h5" className={classes.title}>
+              Popular Books
+            </Typography>
+            {loading && (
+              <div className={classes.skeletonWraper}>
+                {[1, 2, 3, 4, 5].map((n) => (
+                  <BookSkeleton key={n} />
+                ))}
+              </div>
+            )}
+            <Grid container spacing={2}>
+              {popularBooks.map((book) => {
+                book["googleSearch"] = false;
+                return (
+                  <Grid key={book.id} item xs={12} sm={3}>
+                    <BookCard book={book} />
+                  </Grid>
+                );
+              })}
             </Grid>
-          ))}
+          </section>
+
+          <section>
+            <Typography variant="h5" className={classes.title}>
+              Top Rated Books
+            </Typography>
+            {loading && (
+              <div className={classes.skeletonWraper}>
+                {[1, 2, 3, 4, 5].map((n) => (
+                  <BookSkeleton key={n} />
+                ))}
+              </div>
+            )}
+            <Grid container>
+              {topRatedBooks.map((book) => (
+                <Grid key={book.id} item xs={12} sm={3}>
+                  <BookCard book={book} />
+                </Grid>
+              ))}
+            </Grid>
+          </section>
         </Grid>
-      </section>
-      </Grid>
         <Grid item lg={2}>
           <div className={classes.reviews}>
-            <label className={classes.reviewTitle}>
-              Review Feed
-            </label>
-            {
-              reviews.map((review, index) => (
-                <ReviewCardSmall key={index} review={review} />
-              ))
-            }
+            <label className={classes.reviewTitle}>Review Feed</label>
+            {reviews.map((review, index) => (
+              <ReviewCardSmall key={index} review={review} />
+            ))}
           </div>
         </Grid>
       </Grid>
